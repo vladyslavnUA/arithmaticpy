44d7657415bb154bdb0dd9afde8f603d7687e3a1
vladyslavnUA <vladyslavn@yahoo.com>
2020-11-02 01:05:03 -0500

    update

--- arithmaticpy/vlad.py
+++ arithmaticpy/vlad.py
@@ -1,16 +1,14 @@
-def add(one: float, two: float) -> float:
+def add(arg1: float, arg2: float) -> float:
     """
-    Calculate the great circle distance between two points on the 
-    earth (specified in decimal degrees), returns the distance in
-    meters.
-    All arguments must be of equal length.
-    :param lon1: longitude of first place
-    :param lat1: latitude of first place
-    :param lon2: longitude of second place
-    :param lat2: latitude of second place
-    :return: distance in meters between the two sets of coordinates
+    
+    Simple function to add two numbers (float) together
+    All arguments must be of integers or float.
+    :param arg1: first float value
+    :param arg2: second float value
+    :return: sum of the two arguments
+
     """
     
     # get two numbers and add them
-    total = one + two
+    total = arg1 + arg2
     return total
